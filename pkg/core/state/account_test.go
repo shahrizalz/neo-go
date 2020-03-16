d3747ab9592b2f345da4db32aea05d62a499059e
Evgenii Stratonikov <evgeniy@nspcc.ru>
2020-03-16 17:00:09 +0300

    core/state: add benchmarks for different serialization methods
    Compare encoding/gob with io.Serializable.

--- pkg/core/state/account_test.go
+++ pkg/core/state/account_test.go
@@ -1,6 +1,9 @@
 package state
 
 import (
+	"bytes"
+	"encoding/gob"
+	"math/rand"
 	"testing"
 
 	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
@@ -70,4 +73,113 @@ func TestAccountStateBalanceValues(t *testing.T) {
 	bVals := as.GetBalanceValues()
 	assert.Equal(t, util.Fixed8(ref), bVals[asset1])
 	assert.Equal(t, util.Fixed8(ref*10), bVals[asset2])
+}
+
+func BenchmarkAccount_EncodeBinary(b *testing.B) {
+	acc := randomAccount(10)
+
+	b.Run("EncodeBinary", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = encodeIO(acc)
+		}
+	})
+
+	b.Run("gob.Encode", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = encodeGOB(acc)
+		}
+	})
+
+	buf := encodeIO(acc)
+	b.Run("DecodeBinary", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = decodeIO(buf)
+		}
+	})
+
+	buf = encodeGOB(acc)
+	b.Run("gob.Decode", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = decodeGOB(buf)
+		}
+	})
+}
+
+func encodeIO(a *Account) []byte {
+	buf := io.NewBufBinWriter()
+	a.EncodeBinary(buf.BinWriter)
+	if buf.Err != nil {
+		panic(buf.Err)
+	}
+	return buf.Bytes()
+}
+
+func decodeIO(buf []byte) *Account {
+	var a Account
+	r := io.NewBinReaderFromBuf(buf)
+	a.DecodeBinary(r)
+	if r.Err != nil {
+		panic(r.Err)
+	}
+	return &a
+}
+
+func encodeGOB(a *Account) []byte {
+	buf := bytes.NewBuffer(nil)
+	enc := gob.NewEncoder(buf)
+	if err := enc.Encode(a); err != nil {
+		panic(err)
+	}
+	return buf.Bytes()
+}
+
+func decodeGOB(buf []byte) *Account {
+	var a Account
+	r := bytes.NewReader(buf)
+	dec := gob.NewDecoder(r)
+	if err := dec.Decode(&a); err != nil {
+		panic(err)
+	}
+	return &a
+}
+
+func randomAccount(n int) *Account {
+	balances := make(map[util.Uint256][]UnspentBalance)
+	votes := make([]*keys.PublicKey, n)
+	unclaimed := UnclaimedBalances{}
+
+	for i := 0; i < n; i++ {
+		asset := random.Uint256()
+		for j := 0; j < i+1; j++ {
+			balances[asset] = append(balances[asset], UnspentBalance{
+				Tx:    random.Uint256(),
+				Index: uint16(random.Int(0, 65535)),
+				Value: util.Fixed8(int64(random.Int(1, 10000))),
+			})
+		}
+		k, err := keys.NewPrivateKey()
+		if err != nil {
+			panic(err)
+		}
+		votes[i] = k.PublicKey()
+
+		err = unclaimed.Put(&UnclaimedBalance{
+			Tx:    random.Uint256(),
+			Index: uint16(rand.Uint32()),
+			Start: rand.Uint32(),
+			End:   rand.Uint32(),
+			Value: util.Fixed8(rand.Int63()),
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return &Account{
+		ScriptHash: random.Uint160(),
+		IsFrozen:   true,
+		Votes:      votes,
+		Balances:   balances,
+		Unclaimed:  unclaimed,
+	}
 }
